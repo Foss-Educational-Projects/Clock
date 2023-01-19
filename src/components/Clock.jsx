599bff37a463f62c410c2fc4d457b0309c99c369
Foster <72973015+Foster0123@users.noreply.github.com>
2023-01-19 17:58:47 +0530

    Initial Commit 2 Clock

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,39 +1,45 @@
 import React from "react";
-
+import Logo from "./../assets/images/clock-2.png"
 const Clock = () => {
+	const text = 0;
 	return (
 		<div className="clock">
-			<h1>25 + 5 Clock</h1>
+			<h1 className="clock-header">
+			25 + 5 Clock
+			<img id="clock-header-logo" src={Logo} alt="Clock Logo" />
+			</h1>
 			<div className="break-and-session">
 				<div className="break-section">
 					<h3 className="break-section-title">Break</h3>
 					<div className="break-section-settings">
-						<button className="break-increase"><i class="fa-solid fa-plus"></i></button>
-						<p>break-value</p>
-						<button className="break-decrease"><i class="fa-solid fa-minus"></i></button>
+						<button className="break-increase"><i className="fa-solid fa-plus"></i></button>
+						<p className="values">{text}</p>
+						<button className="break-decrease"><i className="fa-solid fa-minus"></i></button>
 					</div>
 				</div>
 				<div className="session-section">
 					<h3 className="session-section-title">Session</h3>
 					<div className="session-section-settings">
-						<button className="session-increase"><i class="fa-solid fa-plus"></i></button>
-						<p>break-value</p>
-						<button className="session-decrease"><i class="fa-solid fa-minus"></i></button>
+						<button className="session-increase"><i className="fa-solid fa-plus"></i></button>
+						<p className="values">{text}</p>
+						<button className="session-decrease"><i className="fa-solid fa-minus"></i></button>
 					</div>
 				</div>
 			</div>
 			<div className="timer">
 				<div className="timer-display">
-					<h2 className="timer-header">Session</h2>
-					<div className="timer-clock">25:00</div>
+					<h2 className="timer-header">Remaining</h2>
+					<div className="timer-clock">
+					25:00
+					</div>
 				</div>
 				<div className="timer-controls">
-					<button className='timer-start'><i class="fa-solid fa-play"></i></button>
-					<button className='timer-pause'><i class="fa-solid fa-pause"></i></button>
-					<button className='timer-reset'><i class="fa-solid fa-arrows-rotate"></i></button>
+					<button className='timer-start'><i className="fa-solid fa-play"></i></button>
+					<button className='timer-pause'><i className="fa-solid fa-pause"></i></button>
+					<button className='timer-reset'><i className="fa-solid fa-arrows-rotate"></i></button>
 				</div>
 			</div>
-			<footer>
+			<footer className="clock-footer">
 				Developed By <strong>Foster Z</strong>
 			</footer>
 		</div>
